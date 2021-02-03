6e8378a23836e8fa2afcfaf73302d913a29b6a75
haruyasu <harukun2002@gmail.com>
2021-02-03 16:29:29 +0900

    インフルエンサーチェック実装

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from .models import Tag, Search
 from django.conf import settings
 from django.utils import timezone
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -49,6 +49,18 @@ def get_hashtag_media(params):
     # endpoint_params['limit'] = 50
     endpoint_params['access_token'] = params['access_token']
     url = params['endpoint_base'] + params['hashtag_id'] + '/top_media'
+    return call_api(url, endpoint_params)
+
+
+# ユーザーアカウント情報取得
+def get_account_info(params):
+    endpoint_params = {}
+    # ユーザ名、プロフィール画像、フォロワー数、フォロー数、投稿数、メディア情報取得
+    endpoint_params['fields'] = 'business_discovery.username(' + params['ig_username'] + '){\
+        username,biography,profile_picture_url,follows_count,followers_count,media_count,\
+        media.limit(10){comments_count,like_count,caption,media_url,permalink,timestamp,media_type}}'
+    endpoint_params['access_token'] = params['access_token']
+    url = params['endpoint_base'] + params['instagram_account_id']
     return call_api(url, endpoint_params)
 
 
@@ -123,3 +135,100 @@ class SearchView(APIView):
             search_serializer = serializers.SearchSerializer(search_data)
             return Response(search_serializer.data)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class AccountView(APIView):
+    def get(self, request):
+        params = get_credentials()
+        access_token = settings.ACCESS_TOKEN
+        instagram_account_id = settings.USER_ID
+        ig_username = 'yumedaruma'
+
+        params['access_token'] = access_token
+        params['instagram_account_id'] = instagram_account_id
+        params['ig_username'] = ig_username
+
+        account_response = get_account_info(params)
+        business_discovery = account_response['json_data']['business_discovery']
+        username = business_discovery['username']
+        biography = business_discovery['biography']
+        profile_picture_url = business_discovery['profile_picture_url']
+        follows_count = business_discovery['follows_count']
+        followers_count = business_discovery['followers_count']
+        media_count = business_discovery['media_count']
+        media_data = business_discovery['media']['data']
+
+        recently_data = []
+        for i in range(6):
+            tags = re.findall('#([^\s→#\ufeff]*)', media_data[i]['caption'])
+            tags = [a for a in tags if a != '']
+            tags = map(lambda x: '#' + x, tags)
+            tags = ' '.join(tags)
+
+            timestamp = (datetime.strptime(media_data[i]['timestamp'], '%Y-%m-%dT%H:%M:%S%z')).strftime("%Y-%m-%d %H:%M")
+
+            recently_data.append({
+                'media_url': media_data[i]['media_url'],
+                'permalink': media_data[i]['permalink'],
+                'timestamp': timestamp,
+                'like_count': media_data[i]['like_count'],
+                'comments_count': media_data[i]['comments_count'],
+                'permalink': media_data[i]['permalink'],
+                'tags': tags
+            })
+
+        # データフレームの作成
+        media_data_frame = pd.DataFrame(media_data, columns=[
+            'comments_count',
+            'like_count',
+            'caption',
+            'media_url',
+            'permalink',
+            'timestamp',
+            'media_type',
+            'id',
+        ])
+
+        row_count = media_data_frame['caption'].str.extractall('#([^\s→#\ufeff]*)').reset_index(level=0).drop_duplicates()[0]
+        hashtag_count = row_count.value_counts().to_dict()
+
+        hashtag_data = []
+        for key, val in hashtag_count.items():
+            post_data = media_data_frame[media_data_frame['caption'].str.contains(key)]
+            hashag_post_data = []
+            average_eng = 0
+            for index, row in post_data.iterrows():
+                timestamp = (datetime.strptime(row['timestamp'], '%Y-%m-%dT%H:%M:%S%z')).strftime("%Y-%m-%d %H:%M")
+
+                hashag_post_data.append({
+                    'media_url': row['media_url'],
+                    'permalink': row['permalink'],
+                    'timestamp': timestamp,
+                    'like_count': row['like_count'],
+                    'comments_count': row['comments_count'],
+                })
+                average_eng += row['like_count'] + row['comments_count']
+
+            average_eng = int(average_eng / val)
+            average_eng_percent = round(average_eng / follows_count, 1)
+
+            hashtag_data.append({
+                'hashtag': key,
+                'post_num': val,
+                'average_eng': average_eng,
+                'average_eng_percent': average_eng_percent,
+                'media': hashag_post_data
+            })
+
+        account_data = {
+            'username': username,
+            'biography': biography,
+            'profile_picture_url': profile_picture_url,
+            'follows_count': follows_count,
+            'followers_count': followers_count,
+            'media_count': media_count,
+            'recently_data': recently_data,
+            'hashtag_data': hashtag_data,
+        }
+
+        return Response(account_data)
